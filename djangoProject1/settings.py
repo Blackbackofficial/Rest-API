454124a7d6edabc3f95f43203ea64eb1c2f873e2
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-09-14 17:23:57 +0300

    Fix heroku

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -9,11 +9,14 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = ''
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,7 +82,7 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
